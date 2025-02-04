8c736039c8f5398b5bf8cfee39e0facf8eeeae34
Omared250 <79874354+Omared250@users.noreply.github.com>
2025-02-04 21:15:00 +0100

    updating port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = parseInt(process.env.PORT || '8080');
+const PORT = parseInt(process.env.PORT || '4000');
 const app = express();
 
 
